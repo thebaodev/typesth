5ad29a3f9e0e84f4bfecd62fdc46fa1ed3d257f0
bao <thebao.developer@gmail.com>
2022-11-15 00:43:17 +0700

    update

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,7 +5,7 @@ import logo from '../assets/images/logo.png';
 const Index = () => (
 	<main className="flex justify-center items-center h-screen w-screen flex-col">
     <header className="flex w-full p-4 justify-start items-end">
-      <a className="flex items-end gap-2">
+      <a href="/" className="flex items-end gap-2">
         <img className="w-6 md:w-8" src={logo} alt="type something endlessly" />
         <h1 className="font-sans text-md lg:text-lg text-base-400">
           typesth endlessly...

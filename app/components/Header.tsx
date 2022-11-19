e8da601f06a60e10f0b3262ef4c0bffae9222158
bao <thebao.developer@gmail.com>
2022-11-19 22:16:09 +0700

    Deploy prod (#12)
    * private beta release (#11)
    
    * 0.0.0-alpha.0
    
    * 0.0.0-alpha.1
    
    * major: alpha release
    
    * 0.0.0
    
    * 0.0.1

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -11,7 +11,7 @@ type HeaderProps = {
 	isShowMenu?: boolean;
 };
 
-const HEADING_TEXT = 'type something endlessly...';
+const HEADING_TEXT = 'typesth.endlessly';
 const Header = forwardRef<HTMLDivElement, HeaderProps>(
 	(
 		{ className = '', isShowHeading = true, isShowMenu = true }: HeaderProps,
@@ -36,53 +36,58 @@ const Header = forwardRef<HTMLDivElement, HeaderProps>(
 		return (
 			<header
 				className={clsx(
-					'grid grid-cols-[auto_1fr] justify-between p-4 h-12',
+					'grid grid-cols-[auto_1fr] justify-between p-4 h-auto md:h-12',
 					className,
 				)}
 			>
-				<a href="/" className="btn btn-ghost rounded-md">
-					<img
-						className="w-6 md:w-8"
-						src={logo}
-						alt="type something endlessly"
-					/>
-				</a>
-				<div className="flex items-center justify-end">
-					<Transition
-						show={isShowHeading}
-						enter="transition-opacity ease-linear  duration-400"
-						enterFrom="opacity-0"
-						enterTo="opacity-100"
-						leave="transition-opacity ease-linear  duration-400"
-						leaveFrom="opacity-100"
-						leaveTo="opacity-0"
-					>
-						<h1
-							ref={headingRef}
-							className="font-sans ml-auto mr-2 text-md lg:text-sm text-base-400"
-						>
-							{HEADING_TEXT.split('').map((char, index) => {
-								return (
-									<span
-										className={clsx('inline-flex', { 'ml-1': !char.trim() })}
-										key={index}
-									>
+				<div className="flex items-center">
+          <a href="/" className="btn btn-ghost rounded-md px-4 mr-2">
+            <img
+              className="w-8 md:w-8"
+              src={logo}
+              alt="typesth endlessly"
+            />
+          </a>
+          <Transition
+            show={isShowHeading}
+            enter="transition-opacity ease-linear  duration-400"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity ease-linear  duration-400"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <h1
+              ref={headingRef}
+              className="font-sans ml-auto mr-2 text-sm md:text-md lg:text-sm text-base-400 opacity-80"
+            >
+              {HEADING_TEXT.split('').map((char, index) => {
+                return (
+                  <span
+                    className={clsx('inline-flex', { 'ml-1': !char.trim() })}
+                    key={index}
+                  >
 										{char}
 									</span>
-								);
-							})}
-						</h1>
-					</Transition>
+                );
+              })}
+            </h1>
+          </Transition>
+        </div>
+				<div className="fixed top-4 right-4">
 					<Transition
 						show={isShowMenu}
-						enter="transition-opacity ease-linear  duration-400"
-						enterFrom="opacity-0"
-						enterTo="opacity-100"
-						leave="transition-opacity ease-linear  duration-400"
-						leaveFrom="opacity-100"
-						leaveTo="opacity-0"
+            appear={true}
+            enter="transition-transform ease-linear duration-800"
+            enterFrom="translate-x-16"
+            enterTo="translate-x-0"
+            leave="transition-transform ease-linear duration-800"
+            leaveFrom="translate-x-0"
+            leaveTo="translate-x-16"
 					>
-						<ThemeSwitcher />
+						<div className="flex gap-1">
+							<ThemeSwitcher />
+						</div>
 					</Transition>
 				</div>
 			</header>

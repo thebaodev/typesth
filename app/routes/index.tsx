1297be0ce9e3d62e488501e8f1ff1fbcb712a8d4
bao <thebao.developer@gmail.com>
2022-11-19 22:00:38 +0700

    major: alpha release

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,12 +4,16 @@ import KBDHint from '~/components/KBDHint';
 import Header from '~/components/Header';
 import Footer from '~/components/Footer';
 import TestResult from '~/components/TestResult';
+import Menu from '~/components/Menu';
+import { TIMER_15, TIMER_ENDLESS } from '~/constant';
 
 const Index = () => {
 	const [isReady, setIsReady] = useState(false);
-	const [isFocus, setIsFocus] = useState(false);
 	const [isTesting, setIsTesting] = useState(false);
 	const [isTested, setIsTested] = useState(false);
+	const [settings, setSettings] = useState<{ timer: number }>({
+		timer: TIMER_15,
+	});
 	const [testData, setTestData] = useState<
 		| {
 				words: string[];
@@ -27,18 +31,10 @@ const Index = () => {
 		setIsTesting(true);
 	};
 
-	const handleStopped = (words: string[], typed: string[], time: number) => {
+	const handleFinished = (words: string[], typed: string[], time: number) => {
 		setIsTesting(false);
 		setIsTested(true);
 		setTestData({ words, typed, time });
-	};
-
-	const handleFocused = () => {
-		setIsFocus(true);
-	};
-
-	const handleBlurred = () => {
-		setIsFocus(false);
 	};
 
 	const handleRestart = () => {
@@ -47,14 +43,28 @@ const Index = () => {
 		setTestData(undefined);
 	};
 
+	const handleTimerSettingChange = (timer: number) => {
+		setSettings({ ...settings, timer });
+	};
+
 	return (
 		<main className="grid grid-rows-[auto_6fr_1fr_auto] grid-cols-[1fr] h-screen w-screen items-center justify-center bg-base-100">
-			<Header isShowHeading={!isTesting} isShowMenu={!isFocus || !isTesting} />
+			<Header isShowHeading={!isTesting} isShowMenu={!isTesting} />
+			<Menu
+				isShow={!isTesting}
+				className="fixed right-2 top-16"
+				callbacks={{
+					onTimerChange: handleTimerSettingChange,
+				}}
+			/>
 			{isTested ? (
 				<TestResult
 					isActive={!!testData}
 					data={testData}
-					className="max-w-screen-xl w-full m-auto p-2 md:p-4 lg:p-5"
+					options={{
+						timer: settings.timer,
+					}}
+					className="max-w-screen-xl w-full m-auto mt-12 p-4 md:p-4 lg:p-8"
 					callbacks={{
 						onRestart: handleRestart,
 					}}
@@ -62,19 +72,23 @@ const Index = () => {
 			) : (
 				<TypeTester
 					isActive={isReady}
-					className="max-w-screen-lg w-full m-auto p-6 md:p-8 lg:p-12"
+					className="max-w-screen-lg w-full m-auto p-6 md:p-6 lg:p-12"
+					options={{
+						fontSize: 56,
+						timer: settings.timer,
+						showLines: 3,
+						extraLimit: 10,
+					}}
 					callbacks={{
 						onStarted: handleStarted,
-						onStopped: handleStopped,
-						onFocus: handleFocused,
-						onBlur: handleBlurred,
+						onFinished: handleFinished,
 						onRestart: handleRestart,
 					}}
 				/>
 			)}
 			<KBDHint
-				isActive={isTesting || isTested || !isFocus}
-				isShowStop={!isTested}
+				isShow={isTesting || isTested}
+				isShowStop={!isTested && settings.timer === TIMER_ENDLESS}
 				className="text-center opacity-40 self-start"
 			/>
 			<Footer isShow={!isTesting} />

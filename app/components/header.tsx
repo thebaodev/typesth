9199751ed7e661703e45b2b0d27c37a555411d0e
bao <thebao.developer@gmail.com>
2022-11-20 19:55:36 +0700

    refactor

---
+++ app/components/header.tsx
@@ -0,0 +1,70 @@
+import React, { forwardRef, useEffect, useRef } from 'react';
+import logo from '~/assets/images/logo.png';
+import clsx from 'clsx';
+import anime from 'animejs';
+import Transition from '~/components/transition';
+import useStore from '~/store';
+import { STATE_RUNNING } from '~/constant';
+
+type HeaderProps = {
+	className?: string;
+	right?: React.ReactNode;
+};
+
+const HEADING_TEXT = 'typesth.endlessly';
+const Header = forwardRef<HTMLDivElement, HeaderProps>(
+	({ className = '', right = undefined }: HeaderProps, ref) => {
+		const { state } = useStore(state => state);
+		const headingRef = useRef<HTMLHeadingElement>(null);
+		const animateHeading = () => {
+			const heading = headingRef.current;
+			const chars = heading?.childNodes;
+			anime({
+				targets: chars,
+				duration: 400,
+				translateY: 2,
+				delay: anime.stagger(100),
+			});
+		};
+
+		useEffect(() => {
+			animateHeading();
+		}, []);
+
+		return (
+			<header
+				className={clsx(
+					'flex items-center justify-between w-full p-4',
+					className,
+				)}
+			>
+				<Transition show={state !== STATE_RUNNING}>
+					<div className="flex items-center">
+						<a href="/" className="btn btn-ghost rounded-md px-4 mr-2">
+							<img className="w-8 md:w-8" src={logo} alt="typesth endlessly" />
+						</a>
+						<h1
+							ref={headingRef}
+							className="font-sans ml-auto mr-2 text-sm md:text-md lg:text-sm text-base-400 opacity-80"
+						>
+							{HEADING_TEXT.split('').map((char, index) => {
+								return (
+									<span
+										className={clsx('inline-flex', { 'ml-1': !char.trim() })}
+										key={index}
+									>
+										{char}
+									</span>
+								);
+							})}
+						</h1>
+					</div>
+				</Transition>
+				{right}
+			</header>
+		);
+	},
+);
+
+Header.displayName = 'Header';
+export default Header;
--- app/constant.ts
+++ app/constant.ts
@@ -1,3 +1,8 @@
+export const STATE_IDLE = 'idle';
+export const STATE_RUNNING = 'running';
+export const STATE_PAUSED = 'paused';
+export const STATE_FINISHED = 'finished';
+
 export const KEYCODES = {
 	ESCAPE: 'Escape',
 	ENTER: 'Enter',
@@ -15,94 +20,42 @@ export const SHORTCUTS = {
 	restart: 'ctrl+enter',
 };
 
-export const THEMES = [
-	{
-		name: 'light',
-		value: 'lofi',
-	},
-	{
-		name: 'dark',
-		value: 'black',
-	},
-	{
-		name: 'cupcake',
-		value: 'cupcake',
-	},
-	{
-		name: 'bumblebee',
-		value: 'bumblebee',
-	},
-	{
-		name: 'emerald',
-		value: 'emerald',
-	},
-	{
-		name: 'corporate',
-		value: 'corporate',
-	},
-	{
-		name: 'synthwave',
-		value: 'synthwave',
-	},
-	{
-		name: 'forest',
-		value: 'forest',
-	},
-	{
-		name: 'aqua',
-		value: 'aqua',
-	},
-
-	{
-		name: 'pastel',
-		value: 'pastel',
-	},
-	{
-		name: 'fantasy',
-		value: 'fantasy',
-	},
-	{
-		name: 'wireframe',
-		value: 'wireframe',
-	},
-	{
-		name: 'luxury',
-		value: 'luxury',
-	},
-	{
-		name: 'dracula',
-		value: 'dracula',
-	},
-
-	{
-		name: 'autumn',
-		value: 'autumn',
-	},
-	{
-		name: 'business',
-		value: 'business',
-	},
-	{
-		name: 'night',
-		value: 'night',
-	},
-	{
-		name: 'coffee',
-		value: 'coffee',
-	},
-	{
-		name: ' winter',
-		value: 'winter',
-	},
-];
+export const THEMES = {
+	light: { value: 'lofi', type: 'light' },
+	dark: { value: 'dark', type: 'dark' },
+	cupcake: { value: 'cupcake', type: 'dark' },
+	bumblebee: { value: 'bumblebee', type: 'light' },
+	emerald: { value: 'emerald', type: 'light' },
+	corporate: { value: 'corporate', type: 'light' },
+	synthwave: { value: 'synthwave', type: 'dark' },
+	forest: { value: 'forest', type: 'dark' },
+	aqua: { value: 'aqua', type: 'light' },
+	pastel: { value: 'pastel', type: 'light' },
+	retro: { value: 'retro', type: 'light' },
+	cyberpunk: { value: 'cyberpunk', type: 'dark' },
+	valentine: { value: 'valentine', type: 'dark' },
+	halloween: { value: 'halloween', type: 'dark' },
+	gatsby: { value: 'gatsby', type: 'dark' },
+	dracula: { value: 'dracula', type: 'dark' },
+	royal: { value: 'royal', type: 'dark' },
+	sunny: { value: 'sunny', type: 'light' },
+	purple: { value: 'purple', type: 'dark' },
+	coffee: { value: 'coffee', type: 'dark' },
+	black: { value: 'black', type: 'dark' },
+	funky: { value: 'funky', type: 'dark' },
+	fantasy: { value: 'fantasy', type: 'light' },
+	wireframe: { value: 'wireframe', type: 'light' },
+	autumn: { value: 'autumn', type: 'light' },
+	winter: { value: 'winter', type: 'light' },
+};
 
 export const TIMER_15 = 15;
 export const TIMER_30 = 30;
 export const TIMER_60 = 60;
 export const TIMER_ENDLESS = 0;
 export const TIMER_OPTIONS = [
-  { value: TIMER_15, label: '15' },
-  { value: TIMER_30, label: '30' },
-  { value: TIMER_60, label: '60' },
-  { value: TIMER_ENDLESS, label: 'endless' },
+	{ value: TIMER_15, label: '15' },
+	{ value: TIMER_30, label: '30' },
+	{ value: TIMER_60, label: '60' },
+	{ value: TIMER_ENDLESS, label: 'endless' },
 ];
